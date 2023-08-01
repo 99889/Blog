1192bd18bcc7d83bb37954cec52ab12d4499e15f
Inshaal Moin <moininshaal7@gmail.com>
2023-08-01 21:50:02 +0530

    third commit

--- My_Blog_project/views.py
+++ My_Blog_project/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
 
 
 
 def index(request):
-    return HttpResponse("Home Page")
+    return HttpResponseRedirect(reverse('App_Blog:blog_list'))
